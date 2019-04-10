45de81a9be5eac25b047b06db0419d3896dd00c4
Ben Overmyer <ben.overmyer@protolabs.com>
2019-04-10 10:48:52 -0500

    Refactor instruments to be even more detailed

--- music.go
+++ music.go
@@ -1,8 +1,9 @@
 package culturegen
 
 import (
+	"bytes"
+	"html/template"
 	"math/rand"
-	"strings"
 
 	"github.com/ironarachne/random"
 )
@@ -14,7 +15,19 @@ type MusicStyle struct {
 	Beat        int
 	Tonality    int
 	Descriptors []string
-	Instruments []string
+	Instruments []Instrument
+}
+
+// Instrument is a musical instrument
+type Instrument struct {
+	Name                   string
+	Description            string
+	Type                   string
+	BaseMaterialOptions    []string
+	SupportMaterialOptions []string
+	BaseMaterial           string
+	SupportMaterial        string
+	DescriptionTemplate    string
 }
 
 func (culture Culture) generateMusicDescriptors() []string {
@@ -67,59 +80,244 @@ func (culture Culture) generateMusicDescriptors() []string {
 	return descriptors
 }
 
-func (culture Culture) generateMusicalInstruments() []string {
-	var instrument string
+func (culture Culture) generateMusicalInstruments() []Instrument {
+	var instrument Instrument
+	var materialType string
+	var availableBaseMaterials []string
+	var availableSupportMaterials []string
 
-	hides := []string{}
-	metals := []string{}
-	woods := []string{}
-	instrumentTypes := []string{}
-	instruments := []string{}
+	availableHides := []string{}
+	availableMetals := []string{}
+	availableWoods := []string{}
+	availableMaterials := []string{}
 
-	for _, r := range culture.HomeClimate.Resources {
-		if r.Type == "metal ingot" {
-			metals = append(metals, strings.TrimSuffix(r.Name, " ingot"))
-		} else if r.Type == "metal bar" {
-			metals = append(metals, strings.TrimSuffix(r.Name, " bar"))
-		} else if r.Type == "wood planks" {
-			woods = append(woods, strings.TrimSuffix(r.Name, " planks"))
-		} else if r.Type == "hide" {
-			hides = append(hides, r.Name)
+	allInstruments := getAllInstruments()
+	availableInstruments := []Instrument{}
+	instruments := []Instrument{}
+
+	for _, i := range culture.HomeClimate.CommonMetals {
+		availableMetals = append(availableMetals, i.Name)
+	}
+
+	for _, i := range culture.HomeClimate.PreciousMetals {
+		availableMetals = append(availableMetals, i.Name)
+	}
+
+	for _, i := range culture.HomeClimate.Plants {
+		if i.IsTree {
+			availableWoods = append(availableWoods, i.Name)
 		}
 	}
 
-	if len(hides) > 0 && len(woods) > 0 {
-		instrumentTypes = append(instrumentTypes, "drum")
-		instrumentTypes = append(instrumentTypes, "bagpipes")
+	for _, i := range culture.HomeClimate.Animals {
+		if i.GivesHide {
+			availableHides = append(availableHides, i.Name)
+		}
 	}
 
-	if len(metals) > 0 {
-		instrumentTypes = append(instrumentTypes, "horn")
-		instrumentTypes = append(instrumentTypes, "pan pipes")
-		instrumentTypes = append(instrumentTypes, "flute")
+	if len(availableHides) > 0 {
+		availableMaterials = append(availableMaterials, "hide")
+	}
+	if len(availableMetals) > 0 {
+		availableMaterials = append(availableMaterials, "metal")
+	}
+	if len(availableWoods) > 0 {
+		availableMaterials = append(availableMaterials, "wood")
 	}
 
-	if len(woods) > 0 {
-		instrumentTypes = append(instrumentTypes, "lyre")
-		instrumentTypes = append(instrumentTypes, "lute")
+	for _, i := range allInstruments {
+		if slicePartlyWithin(i.BaseMaterialOptions, availableMaterials) {
+			if slicePartlyWithin(i.SupportMaterialOptions, availableMaterials) {
+				availableInstruments = append(availableInstruments, i)
+			}
+		}
 	}
-
-	baseMaterials := metals
-	baseMaterials = append(baseMaterials, woods...)
 
 	numberOfInstruments := rand.Intn(3) + 1
 
 	for i := 0; i < numberOfInstruments; i++ {
-		instrument = random.Item(instrumentTypes)
-		if instrument == "drum" {
-			instrument = random.Item(woods) + " drum skinned with " + random.Item(hides)
-		} else if instrument == "lyre" || instrument == "lute" {
-			instrument = random.Item(woods) + " " + instrument
-		} else {
-			instrument = random.Item(baseMaterials) + " " + instrument
+		instrument = availableInstruments[rand.Intn(len(availableInstruments)-1)]
+		availableBaseMaterials = []string{}
+		availableSupportMaterials = []string{}
+
+		for _, m := range instrument.BaseMaterialOptions {
+			if inSlice(m, availableMaterials) {
+				availableBaseMaterials = append(availableBaseMaterials, m)
+			}
 		}
 
+		for _, m := range instrument.SupportMaterialOptions {
+			if inSlice(m, availableMaterials) {
+				availableSupportMaterials = append(availableSupportMaterials, m)
+			}
+		}
+
+		materialType = random.Item(availableBaseMaterials)
+		if materialType == "hide" {
+			instrument.BaseMaterial = random.Item(availableHides)
+		} else if materialType == "metal" {
+			instrument.BaseMaterial = random.Item(availableMetals)
+		} else if materialType == "wood" {
+			instrument.BaseMaterial = random.Item(availableWoods)
+		}
+
+		materialType = random.Item(availableSupportMaterials)
+		if materialType == "hide" {
+			instrument.SupportMaterial = random.Item(availableHides)
+		} else if materialType == "metal" {
+			instrument.SupportMaterial = random.Item(availableMetals)
+		} else if materialType == "wood" {
+			instrument.SupportMaterial = random.Item(availableWoods)
+		}
+
+		instrument.Description = instrument.getDescription()
+
 		instruments = append(instruments, instrument)
+	}
+
+	return instruments
+}
+
+func (instrument Instrument) getDescription() string {
+	t := template.New("instrument description")
+
+	var err error
+	t, err = t.Parse(instrument.DescriptionTemplate)
+	if err != nil {
+		panic(err)
+	}
+
+	var tpl bytes.Buffer
+	if err := t.Execute(&tpl, instrument); err != nil {
+		panic(err)
+	}
+
+	result := tpl.String()
+
+	return result
+}
+
+func getAllInstruments() []Instrument {
+	instruments := []Instrument{
+		Instrument{
+			Name:                   "short flute",
+			Type:                   "flute",
+			BaseMaterialOptions:    []string{"metal", "wood"},
+			SupportMaterialOptions: []string{"metal", "wood"},
+			DescriptionTemplate:    "{{.BaseMaterial}} {{.Name}} trimmed with {{.SupportMaterial}}",
+		},
+		Instrument{
+			Name:                   "long flute",
+			Type:                   "flute",
+			BaseMaterialOptions:    []string{"metal", "wood"},
+			SupportMaterialOptions: []string{"metal", "wood"},
+			DescriptionTemplate:    "{{.BaseMaterial}} {{.Name}} trimmed with {{.SupportMaterial}}",
+		},
+		Instrument{
+			Name:                   "twin flute",
+			Type:                   "flute",
+			BaseMaterialOptions:    []string{"metal", "wood"},
+			SupportMaterialOptions: []string{"metal", "wood"},
+			DescriptionTemplate:    "{{.BaseMaterial}} {{.Name}} trimmed with {{.SupportMaterial}}",
+		},
+		Instrument{
+			Name:                   "short harp",
+			Type:                   "harp",
+			BaseMaterialOptions:    []string{"metal", "wood"},
+			SupportMaterialOptions: []string{"hide"},
+			DescriptionTemplate:    "{{.BaseMaterial}} {{.Name}} strung with {{.SupportMaterial}} sinew",
+		},
+		Instrument{
+			Name:                   "long harp",
+			Type:                   "harp",
+			BaseMaterialOptions:    []string{"metal", "wood"},
+			SupportMaterialOptions: []string{"hide"},
+			DescriptionTemplate:    "{{.BaseMaterial}} {{.Name}} strung with {{.SupportMaterial}} sinew",
+		},
+		Instrument{
+			Name:                   "full harp",
+			Type:                   "harp",
+			BaseMaterialOptions:    []string{"metal", "wood"},
+			SupportMaterialOptions: []string{"hide"},
+			DescriptionTemplate:    "{{.BaseMaterial}} {{.Name}} strung with {{.SupportMaterial}} sinew",
+		},
+		Instrument{
+			Name:                   "lyre",
+			Type:                   "lyre",
+			BaseMaterialOptions:    []string{"wood"},
+			SupportMaterialOptions: []string{"hide"},
+			DescriptionTemplate:    "{{.BaseMaterial}} {{.Name}} strung with {{.SupportMaterial}} sinew",
+		},
+		Instrument{
+			Name:                   "lijerica",
+			Type:                   "lyre",
+			BaseMaterialOptions:    []string{"wood"},
+			SupportMaterialOptions: []string{"hide"},
+			DescriptionTemplate:    "{{.BaseMaterial}} {{.Name}} strung with {{.SupportMaterial}} sinew",
+		},
+		Instrument{
+			Name:                   "long-necked lute",
+			Type:                   "lute",
+			BaseMaterialOptions:    []string{"wood"},
+			SupportMaterialOptions: []string{"hide"},
+			DescriptionTemplate:    "{{.BaseMaterial}} {{.Name}} strung with {{.SupportMaterial}} sinew",
+		},
+		Instrument{
+			Name:                   "pierced lute",
+			Type:                   "lute",
+			BaseMaterialOptions:    []string{"wood"},
+			SupportMaterialOptions: []string{"hide"},
+			DescriptionTemplate:    "{{.BaseMaterial}} {{.Name}} strung with {{.SupportMaterial}} sinew",
+		},
+		Instrument{
+			Name:                   "short-necked lute",
+			Type:                   "lute",
+			BaseMaterialOptions:    []string{"wood"},
+			SupportMaterialOptions: []string{"hide"},
+			DescriptionTemplate:    "{{.BaseMaterial}} {{.Name}} strung with {{.SupportMaterial}} sinew",
+		},
+		Instrument{
+			Name:                   "single-drone bagpipes",
+			Type:                   "bagpipes",
+			BaseMaterialOptions:    []string{"hide"},
+			SupportMaterialOptions: []string{"metal", "wood"},
+			DescriptionTemplate:    "{{.BaseMaterial}}-hide {{.Name}} with {{.SupportMaterial}} drone",
+		},
+		Instrument{
+			Name:                   "multiple-drone bagpipes",
+			Type:                   "bagpipes",
+			BaseMaterialOptions:    []string{"hide"},
+			SupportMaterialOptions: []string{"metal", "wood"},
+			DescriptionTemplate:    "{{.BaseMaterial}}-hide {{.Name}} with {{.SupportMaterial}} drones",
+		},
+		Instrument{
+			Name:                   "hand drum",
+			Type:                   "drum",
+			BaseMaterialOptions:    []string{"hide"},
+			SupportMaterialOptions: []string{"metal", "wood"},
+			DescriptionTemplate:    "{{.BaseMaterial}} {{.Name}} skinned with {{.SupportMaterial}} hide",
+		},
+		Instrument{
+			Name:                   "short drum",
+			Type:                   "drum",
+			BaseMaterialOptions:    []string{"hide"},
+			SupportMaterialOptions: []string{"metal", "wood"},
+			DescriptionTemplate:    "{{.BaseMaterial}} {{.Name}} skinned with {{.SupportMaterial}} hide",
+		},
+		Instrument{
+			Name:                   "walking drum",
+			Type:                   "drum",
+			BaseMaterialOptions:    []string{"hide"},
+			SupportMaterialOptions: []string{"metal", "wood"},
+			DescriptionTemplate:    "{{.BaseMaterial}} {{.Name}} skinned with {{.SupportMaterial}} hide",
+		},
+		Instrument{
+			Name:                   "heavy drum",
+			Type:                   "drum",
+			BaseMaterialOptions:    []string{"hide"},
+			SupportMaterialOptions: []string{"metal", "wood"},
+			DescriptionTemplate:    "{{.BaseMaterial}} {{.Name}} skinned with {{.SupportMaterial}} hide",
+		},
 	}
 
 	return instruments
--- utility.go
+++ utility.go
@@ -9,3 +9,31 @@ func inSlice(item string, collection []string) bool {
 	}
 	return false
 }
+
+// sliceWithin checks to see if a slice of strings is wholly within another slice of strings
+func sliceWithin(slice1 []string, slice2 []string) bool {
+	if len(slice1) == 0 {
+		return true
+	}
+
+	for _, i := range slice1 {
+		if !inSlice(i, slice2) {
+			return false
+		}
+	}
+	return true
+}
+
+// slicePartlyWithin checks to see if at least one element of a slice is in the second slice
+func slicePartlyWithin(slice1 []string, slice2 []string) bool {
+	if len(slice1) == 0 {
+		return true
+	}
+
+	for _, i := range slice1 {
+		if inSlice(i, slice2) {
+			return true
+		}
+	}
+	return false
+}
